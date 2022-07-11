20d827baf91cf16b3d7a99c42dd07010ccc1c6cc
AbigailBlanco-davinci <86854315+abiblanco@users.noreply.github.com>
2022-07-10 23:15:04 -0300

    Add files via upload

---
+++ Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Usuario
+    {
+        public int id;
+        public string name;
+        public string email;
+
+        public Usuario(int id, string name, string email)
+        {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+        }
+    }
+}
